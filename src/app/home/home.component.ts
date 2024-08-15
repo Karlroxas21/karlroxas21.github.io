7a5e9ef0451d7c8e2dd695b1742aa996d3c0fd4b
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2024-08-15 14:34:17 +0800

    fix DOM issue

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
 import { FooterComponent } from "../footer/footer.component";
 import Typed from 'typed.js';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [FooterComponent],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    imports: [FooterComponent],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
 })
 export class HomeComponent implements OnInit {
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
+
     ngOnInit(): void {
-        const options = {
-            strings: ['currently looking for a job', 'do back-end development', 'do front-end development', 'specialized in Web Development'],
-            typeSpeed: 50,
-            backSpeed: 50,
-            loop: true
-          };
-          const typed = new Typed('.test-component', options);
+        if (isPlatformBrowser(this.platformId)) {
+            const options = {
+                strings: ['currently looking for a job', 'do back-end development', 'do front-end development', 'specialized in Web Development'],
+                typeSpeed: 50,
+                backSpeed: 50,
+                loop: true
+            };
+            const typed = new Typed('.test-component', options);
+        }
+
     }
 
 }
