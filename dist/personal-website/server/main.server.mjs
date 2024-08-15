61ed7c9c3a2fa5abe2760fbf92c68ba35785422a
Karl Marx Roxas <karlmarxroxas1@gmail.com>
2024-08-15 13:19:42 +0800

    attempt to fix markdown render bug

--- dist/personal-website/server/main.server.mjs
+++ dist/personal-website/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RQF6PB7D.mjs";import"./chunk-XVQRUYYC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-DXB56F5S.mjs";import"./chunk-DXNGXEKC.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideMarkdown } from 'ngx-markdown';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideMarkdown()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes, withHashLocation()), provideClientHydration(), provideMarkdown()]
 };
