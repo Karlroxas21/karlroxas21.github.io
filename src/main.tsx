6fc9c737c68f3173e20349604e42947234316162
Karl Marx Roxas <karlm.roxas@gmail.com>
2026-03-07 15:57:21 +0800

    chore: use hash router instead

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router';
+import { HashRouter } from 'react-router';
 import { AppRoutes } from './routes';
 
-// Handle GitHub Pages 404 redirects
-const redirect = sessionStorage.redirect;
-if (redirect) {
-    delete sessionStorage.redirect;
-    window.history.replaceState(null, '', redirect);
-}
-
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
+    <HashRouter>
         <StrictMode>
             <AppRoutes />
         </StrictMode>
-    </BrowserRouter>
+    </HashRouter>
 );
